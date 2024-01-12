31442f48bcd98051a520514afbadf7d2221c2666
Adam Hillo <adam1hillo@gmail.com>
2024-01-12 15:33:08 +0100

    BMI voorbeeld

---
+++ src/be/vdab/jpfhfdst5/Bmi.java
@@ -0,0 +1,13 @@
+package be.vdab.jpfhfdst5;
+
+
+public class Bmi {
+    public static void main(String[] args) {
+        float gewicht = 75.7F;
+        float lengte = 1.70F;
+        float bmi;
+
+        bmi = gewicht / (lengte * lengte);
+        System.out.println("De bodymassindex is " + bmi);
+    }
+}
