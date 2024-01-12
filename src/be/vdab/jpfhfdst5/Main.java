63d56f7f603b71374a7ee61604a3c07cd53decbf
Adam Hillo <adam1hillo@gmail.com>
2024-01-12 15:43:07 +0100

    TEMP voorbeeld

---
+++ src/be/vdab/jpfhfdst5/Main.java
@@ -0,0 +1,10 @@
+package be.vdab.jpfhfdst5;
+
+public class Main {
+    public static void main(String[] args) {
+        float tempCelsius = 37.0F;
+        float tempFahr = tempCelsius * 9 / 5 + 32;
+        System.out.println("Temperatuur in graden Celsius: " + tempCelsius);
+        System.out.println("Temperatuur in graden Fahrenheit " + tempFahr);
+    }
+}
